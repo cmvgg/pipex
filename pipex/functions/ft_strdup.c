11daf2144b49d2e1d1392f2c1d932e4d71279dd1
César Manuel Varela García <113544540+cmvgg@users.noreply.github.com>
2023-12-20 22:06:56 +0100

    subida 1

---
+++ pipex/functions/ft_strdup.c
@@ -0,0 +1,19 @@
+#include "../includes/pipex.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*cp;
+	size_t	i;
+
+	i = 0;
+	cp = (char *)malloc(ft_strlen(src) + 1);
+	if (!cp)
+		return (NULL);
+	while (src[i])
+	{
+		cp[i] = src[i];
+		i++;
+	}
+	cp[i] = '\0';
+	return (cp);
+}
